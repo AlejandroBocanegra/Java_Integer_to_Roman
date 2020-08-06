b63cd1bf9a97d27b8c61288768ce531ff1385146
AlejandroBocanegra <a.boca@outlook.com>
2020-08-06 22:33:51 +0900

    created proj, instructions

---
+++ src/main/java/Java_Integer_to_Roman/IntegerToRoman.java
@@ -0,0 +1,14 @@
+package Java_Integer_to_Roman;
+
+public class IntegerToRoman {
+    // create objects for 1's, 10's, 100s
+    // create string array
+    // break up the integer and place into an array
+    // array.length to find out where to start, 1K = M, >1K = "too big"
+    // match value at index to key in order to output roman numeral
+    // add roman number to empty string array
+    // string array to string
+    // return
+
+    
+}
